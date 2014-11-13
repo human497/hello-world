fd34a35f1149158b0208f1a275a0e30640e6af21
Richard Carr <richjcarr@yahoo.com>
2014-11-13 10:58:18 -0500

    Added c program
    Added a hello world program written in C.

---
+++ test.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : test.c
+ Author      : test
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	printf("Feth, i am writing things.");
+	return EXIT_SUCCESS;
+}
